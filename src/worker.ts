c1bd43d84346c0330e765494888c83d6d731e7ea
Van Paul Dayag <vcdayag@up.edu.ph>
2023-05-27 21:36:17 +0800

    feat: add caching

--- src/worker.ts
+++ src/worker.ts
@@ -1,60 +1,82 @@
 export default {
-  async fetch(request: Request, env: Env) {
-    const url = new URL(request.url);
-    const key = url.pathname.slice(1);
+  async fetch(request: Request, env: Env, context: ExecutionContext) {
+    try {
+      const url = new URL(request.url);
+      const key = url.pathname.slice(1);
 
-    switch (request.method) {
-      case 'PUT':
-        await env.STALS_BUCKET.put(key, request.body);
-        return new Response(`Put ${key} successfully!`);
-      case 'GET':
-        const rpath = key.split("/");
+      switch (request.method) {
+        case 'PUT':
+          await env.STALS_BUCKET.put(key, request.body);
+          return new Response(`Put ${key} successfully!`);
+        case 'GET':
+          // Construct the cache key from the cache URL
+          const cacheKey = new Request(url.toString(), request);
+          const cache = caches.default;
 
-        if (rpath.length == 2) {
-          let options: R2ListOptions = {
-            prefix: rpath[1],
-            limit: 3
+          // Check whether the value is already available in the cache
+          // if not, you will need to fetch it from R2, and store it in the cache
+          // for future access
+          let response = await cache.match(cacheKey);
+
+          if (response) {
+            console.log(`Cache hit for: ${request.url}.`);
+            return response;
           }
-          switch (rpath[0]) {
-            case "getOne":
-              options.limit = 1;
-              break;
-            case "getAll":
-              break;
+
+          const rpath = key.split("/");
+
+          if (rpath.length == 2) {
+            let options: R2ListOptions = {
+              prefix: rpath[1],
+              limit: 3
+            }
+            switch (rpath[0]) {
+              case "getOne":
+                options.limit = 1;
+                break;
+              case "getAll":
+                break;
+            }
+
+            const obj = await env.STALS_BUCKET.list(options);
+            let list: string[] = [];
+            obj.objects.forEach(element => {
+              list.push("https://" + url.hostname + "/" + element.key);
+            });
+            const res = new Response(JSON.stringify(list));
+            return res;
+          }
+          const object = await env.STALS_BUCKET.get(key);
+
+          if (object === null) {
+            return new Response('Object Not Found', { status: 404 });
           }
 
-          const obj = await env.STALS_BUCKET.list(options);
-          let list: string[] = [];
-          obj.objects.forEach(element => {
-            list.push("https://" + url.hostname + "/" + element.key);
+          const headers = new Headers();
+          object.writeHttpMetadata(headers);
+          headers.set('etag', object.httpEtag);
+
+          const GETImageResponse = new Response(object.body, {
+            headers,
           });
-          const res = new Response(JSON.stringify(list));
-          return res;
-        }
-        const object = await env.STALS_BUCKET.get(key);
 
-        if (object === null) {
-          return new Response('Object Not Found', { status: 404 });
-        }
+          context.waitUntil(cache.put(cacheKey, GETImageResponse.clone()));
 
-        const headers = new Headers();
-        object.writeHttpMetadata(headers);
-        headers.set('etag', object.httpEtag);
+          return GETImageResponse;
+        case 'DELETE':
+          await env.STALS_BUCKET.delete(key);
+          return new Response('Deleted!');
 
-        return new Response(object.body, {
-          headers,
-        });
-      case 'DELETE':
-        await env.STALS_BUCKET.delete(key);
-        return new Response('Deleted!');
-
-      default:
-        return new Response('Method Not Allowed', {
-          status: 405,
-          headers: {
-            Allow: 'PUT, GET, DELETE',
-          },
-        });
+        default:
+          return new Response('Method Not Allowed', {
+            status: 405,
+            headers: {
+              Allow: 'PUT, GET, DELETE',
+            },
+          });
+      }
+    } catch (e) {
+      return new Response('Error thrown ' + e.message);
     }
   },
 };
